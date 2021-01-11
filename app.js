6c0978fce90184af053714e43bbe01a7c4bded35
Joshua Mba <angelicjoekeyz@gmail.com>
2021-01-11 08:49:38 +0100

    Feat: Completed the server setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const app = express()
+
+
+
+//middleware
+app.use(express.json())
+
+
+//user route
+
+
+//course route
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app 
