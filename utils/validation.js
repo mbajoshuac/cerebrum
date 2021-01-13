1896ce1dd42f0a320b8526d7a744d83b29a0dd7a
Joshua Mba <angelicjoekeyz@gmail.com>
2021-01-13 09:20:16 +0100

    Feat: Completed the utils files

---
+++ utils/validation.js
@@ -0,0 +1,17 @@
+const Joi = require('joi')
+
+///validation for registration
+exports.regValidation = Joi.object({
+    firstName: Joi.string().min(3).max(15).required(),
+    lastName: Joi.string().min(3).max(15).required(),
+    phoneNumber: Joi.string().min(3).max(15).required(),
+    email: Joi.string().min(5).max(45).required(),
+    country: Joi.string().min(3).max(45).required(),
+    password: Joi.string().min(2).max(55).required(),
+});
+
+//Validation for Login
+exports.loginValidation = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+})
